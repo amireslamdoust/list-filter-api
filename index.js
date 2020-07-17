f0d26337ad982c4540fd0c1d6dc0bf7ea27d87f1
amireslamdoust <amireslamdoust@gmail.com>
2020-07-17 07:27:34 +0200

    cros

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express')
+var cors = require('cors')
 const app = express()
 
+app.use(cors())
 app.get('/', function (req, res) {
+
 
     const faker = require('faker');
     const list = [];
