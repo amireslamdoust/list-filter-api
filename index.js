ffac9f7199e9a6d9b341a0c326e11893969c0553
amireslamdoust <amireslamdoust@gmail.com>
2020-11-20 00:46:34 +0100

    api

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express')
-var cors = require('cors')
+let cors = require('cors')
 const app = express()
 
 app.use(cors())
@@ -8,109 +8,29 @@ app.get('/', function (req, res) {
 
     const faker = require('faker');
     const list = [];
-    const limit = 24
-    const maxPage = 7
-    const page = req.query.page || 1
-
-    let requestRate = req.query.rate
-    if (requestRate && (requestRate < 1 || requestRate > 5)) {
-        requestRate = undefined
-    }
-    if (requestRate) {
-        requestRate = parseFloat(requestRate).toFixed(1)
-    }
-
-    let reviewSort = req.query.reviewSort
-    if (reviewSort && (reviewSort === 'asc' || reviewSort === 'desc')) {
-        reviewSort = reviewSort
-    } else {
-        reviewSort = undefined
-    }
-
-    let currentStatus = req.query.status
-    if (currentStatus && (currentStatus === 'online' || currentStatus === 'offline')) {
-        currentStatus = currentStatus
-    } else {
-        currentStatus = undefined
-    }
-
-    const maxReview = 10000
-
-    function getRandomArbitrary(min, max) {
-        const rand = Math.random() * (max - min) + min;
-        return Math.floor(rand)
-    }
-
-    const defaultLocales = ["de", "en", "fr", "it"]
-
-    let locales = []
-
-    req.query.locales && Object.values(req.query.locales).forEach(l => {
-       if(defaultLocales.includes(l)) {
-           locales.push(l)
-       }
-    })
-    if(locales.length < 1) {
-        locales = defaultLocales
-    }
+    const limit = 30
 
 
     for (let i = 0; i < limit; i++) {
 
-        let minRate = 0.0
-        if (requestRate) {
-            minRate = parseFloat(requestRate).toFixed(1)
-        }
-
-        const rate = (Math.random() * (minRate - 5.0) + 5.0).toFixed(1)
-        const locale = []
-        const rand = Math.floor(Math.random() * locales.length) + 1
-        for (let j = 0; j < rand; j++) {
-            const index = locales[Math.floor(Math.random() * locales.length)]
-            if (!locale.includes(index)) {
-                locale.push(index)
-            }
-        }
-        const status = currentStatus ? currentStatus : Math.floor(Math.random() * 2) ? 'online' : ' offline'
-        let reviews = getRandomArbitrary(0, maxReview);
-        if (reviewSort) {
-            if (reviewSort === 'asc') {
-                if (list[i - 1]) {
-                    reviews = getRandomArbitrary(list[i - 1]['reviews'], maxReview)
-                }
-            }
-
-            if (reviewSort === 'desc') {
-                if (list[i - 1]) {
-                    reviews = getRandomArbitrary(0, list[i - 1]['reviews'])
-                }
-            }
-        }
+        const rate = (Math.random() *  5).toFixed(1)
+        // const rate = (Math.random() *  5).toFixed(0)
 
         const item = {
             name: faker.name.findName(),
-            email: faker.internet.email(),
-            image: faker.internet.avatar(),
-            locale: locale,
-            rate: rate,
-            status: status,
-            reviews: reviews
+            image: faker.image.imageUrl(),
+            address: faker.address.streetAddress(),
+            rate:  rate
         }
         list.push(item)
     }
     res.setHeader('Content-Type', 'application/json');
-    res.end(JSON.stringify({
-        totalItems: limit * maxPage,
-        currentPage: page,
-        locales: locales,
-        rate: requestRate,
-        reviewSort: reviewSort,
-        currentStatus: currentStatus,
-        list: list
-    }));
+    res.end(JSON.stringify(
+        list
+    ));
 
 })
 
-const server = app.listen(8000, function () {
+const server = app.listen(8001, function () {
     console.log('Server running at http://localhost:' + server.address().port)
 })
